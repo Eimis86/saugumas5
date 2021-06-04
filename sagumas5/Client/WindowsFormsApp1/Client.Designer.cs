7feac6a8bcc3f66c19cba7d9e9bfe1d2cc073d5e
Eimis86 <75447681+Eimis86@users.noreply.github.com>
2021-06-04 14:29:23 +0300

    Add files via upload

---
+++ sagumas5/Client/WindowsFormsApp1/Client.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.ConnectBtn = new System.Windows.Forms.Button();
+            this.messageTxt = new System.Windows.Forms.RichTextBox();
+            this.SendMessage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.Location = new System.Drawing.Point(15, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ip Address:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(104, 30);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label2.Location = new System.Drawing.Point(297, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port:";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox12.Location = new System.Drawing.Point(345, 31);
+            this.textBox12.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(155, 26);
+            this.textBox12.TabIndex = 3;
+            // 
+            // ConnectBtn
+            // 
+            this.ConnectBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConnectBtn.Location = new System.Drawing.Point(508, 30);
+            this.ConnectBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ConnectBtn.Name = "ConnectBtn";
+            this.ConnectBtn.Size = new System.Drawing.Size(143, 27);
+            this.ConnectBtn.TabIndex = 4;
+            this.ConnectBtn.Text = "Connect";
+            this.ConnectBtn.UseVisualStyleBackColor = true;
+            this.ConnectBtn.Click += new System.EventHandler(this.ConnectBtn_Click);
+            // 
+            // messageTxt
+            // 
+            this.messageTxt.Location = new System.Drawing.Point(20, 72);
+            this.messageTxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.messageTxt.Name = "messageTxt";
+            this.messageTxt.Size = new System.Drawing.Size(631, 201);
+            this.messageTxt.TabIndex = 5;
+            this.messageTxt.Text = "";
+            // 
+            // SendMessage
+            // 
+            this.SendMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SendMessage.Location = new System.Drawing.Point(506, 281);
+            this.SendMessage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.SendMessage.Name = "SendMessage";
+            this.SendMessage.Size = new System.Drawing.Size(145, 32);
+            this.SendMessage.TabIndex = 9;
+            this.SendMessage.Text = "Send";
+            this.SendMessage.UseVisualStyleBackColor = true;
+            this.SendMessage.Click += new System.EventHandler(this.SendMessage_Click);
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 327);
+            this.Controls.Add(this.SendMessage);
+            this.Controls.Add(this.messageTxt);
+            this.Controls.Add(this.ConnectBtn);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Button ConnectBtn;
+        private System.Windows.Forms.RichTextBox messageTxt;
+        private System.Windows.Forms.Button SendMessage;
+    }
+}
+
---
+++ sagumas5/Client/WindowsFormsApp1/Client.cs
@@ -0,0 +1,57 @@
+﻿using Server;
+using SimpleTCP;
+using System;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Client : Form
+    {
+        private SimpleTcpClient client;
+
+        public Client()
+        {
+            InitializeComponent();
+            InitNewClient();
+        }
+
+        private void InitNewClient()
+        {
+            textBox1.Text = "127.0.0.1";
+            textBox12.Text = "2020";
+            client = new SimpleTcpClient();
+            client.StringEncoder = Encoding.UTF8;
+            client.DataReceived += DataReceived;
+        }
+
+        private void ConnectBtn_Click(object sender, EventArgs e)
+        {
+
+                client.Connect(textBox1.Text, Convert.ToInt32(textBox12.Text));
+            
+        }
+
+        private void SendMessage_Click(object sender, EventArgs e)
+        {
+                byte[] signature = Signature.Signature1(messageTxt.Text);
+
+                string clientSignature = Encoding.UTF8.GetString(signature);
+                string clientMsg = messageTxt.Text;
+                //ServerSignature
+                HelperMethods.CallExtraMethod(clientMsg, signature);
+
+                client.Write(clientMsg);
+
+        }
+
+        private void DataReceived(Object sender, SimpleTCP.Message msg)
+        {
+           /* statusTxt.Invoke((MethodInvoker)delegate
+            {
+                statusTxt.Text = msg.MessageString;
+            });*/
+        }
+    }
+}
